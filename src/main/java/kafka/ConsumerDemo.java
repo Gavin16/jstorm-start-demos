faf118b1518e95d7afca67b2104a543c9388c36d
Minsky <mingorun@163.com>
2019-01-31 12:13:26 +0800

    sync

--- src/main/java/kafka/ConsumerDemo.java
+++ src/main/java/kafka/ConsumerDemo.java
@@ -20,13 +20,16 @@ public class ConsumerDemo {
 
         // server, group.id, key.deserializer, value.deserializer四个参数无默认值，必须配置
         // 注意这里 服务器地址配置的 主机名:端口号, 需要在研发环境修改hosts 文件
-        props.put("bootstrap.servers","ubuntu3:9092");
+        props.put("bootstrap.servers","ubuntu1:9092");
         props.put("group.id",groupId);
         props.put("key.deserializer","org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer","org.apache.kafka.common.serialization.StringDeserializer");
-        //
+        // 是否允许consumer 位移自动提交
         props.put("enable.auto.commit","true");
+        // consumer 位移自动提交时间间隔
         props.put("auto.commit.interval.ms","1000");
+        // auto.offset.reset 设置为 earliest 指定从最早的位移开始消费，但是如果之前有位移提交,则启动时从位移提交处开始消费
+        // auto.offset.reset 通常还可以设置为 latest, 设置为latest 指的从最新处位移开始消费
         props.put("auto.offset.reset","earliest");
 
         KafkaConsumer<String,String> consumer = new KafkaConsumer<String, String>(props);
@@ -34,7 +37,7 @@ public class ConsumerDemo {
 
         try {
             while(true){
-                ConsumerRecords<String,String> records = consumer.poll(1000);
+                ConsumerRecords<String,String> records = consumer.poll(2000);
                 for(ConsumerRecord<String,String> record : records){
                     System.out.printf("订阅消息 offset=%d,key=%s,value=%s%n",record.offset(),record.key(),record.value());
                 }
